34b5254bd515c18ed196e4355ba0fba954d9392f
bytixo <69140663+bytixo@users.noreply.github.com>
2021-11-19 21:23:33 +0100

    Add files via upload

---
+++ cars-assemble/cars_assemble.go
@@ -0,0 +1,38 @@
+package cars
+
+// SuccessRate is used to calculate the ratio of an item being created without
+// error for a given speed
+func SuccessRate(speed int) float64 {
+
+	switch {
+	case speed == 0:
+		return 0
+	case speed <= 4:
+		return 1
+	case speed <= 8:
+		return .9
+	default:
+		return .77
+	}
+}
+
+// CalculateProductionRatePerHour for the assembly line, taking into account
+// its success rate
+func CalculateProductionRatePerHour(speed int) float64 {
+	return float64(speed*221) * SuccessRate(speed)
+}
+
+// CalculateProductionRatePerMinute describes how many working items are
+// produced by the assembly line every minute
+func CalculateProductionRatePerMinute(speed int) int {
+	return int(CalculateProductionRatePerHour(speed) / 60)
+}
+
+// CalculateLimitedProductionRatePerHour describes how many working items are
+// produced per hour with an upper limit on how many can be produced per hour
+func CalculateLimitedProductionRatePerHour(speed int, limit float64) float64 {
+	if CalculateProductionRatePerHour(speed) > limit {
+		return limit
+	}
+	return CalculateProductionRatePerHour(speed)
+}
